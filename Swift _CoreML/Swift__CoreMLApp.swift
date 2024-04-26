e6923f6205d399179333d6b349c79274b10406c8
shoki iwashita <iwashitashoki@gmail.com>
2024-04-26 10:15:24 +0900

    Initial Commit

---
+++ Swift _CoreML/Swift__CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift__CoreMLApp.swift
+//  Swift _CoreML
+//
+//  Created by syoki iwasyita on 2024/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift__CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
